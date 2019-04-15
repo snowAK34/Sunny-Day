260bb5aa760a9ef690c99821bc1cb7b79d0e48f3
Nielsen <jeanielsen@comcast.net>
2019-04-15 08:24:50 -0700

    change for security

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL){
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Jlmyhd60*',
+    password: '',
     database: 'Sunny-Day_db'
   })
 }
