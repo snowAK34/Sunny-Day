85eb320d9fc0e39186c048ec6f9758b63792cd06
personal computer <bret.allan@gmail.com>
2019-04-17 18:13:42 -0700

    added styling, added signup link to home and protected signup page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,7 +14,7 @@ router.post("/", passport.authenticate("local"), function(req, res) {
 // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
 // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
 // otherwise send back an error
-router.post("/api/signup", function(req, res) {
+router.post("/api/signup", isAuthenticated, function(req, res) {
   db.User.create({
     email: req.body.email,
     password: req.body.password
